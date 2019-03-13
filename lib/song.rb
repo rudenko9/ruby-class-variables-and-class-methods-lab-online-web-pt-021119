031c5c9ed2d442cf1dc61c12f8fee143981fa3cb
Nataliya R <n.rudenko@gmail.com>
2019-03-13 02:31:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,9 @@ class Song
 end 
 def self.artist_count
   artist_count = Hash.new(0)
-  @@artists.each {|artist| artist_count[artist] += 1 }
+  @@artists.each do |artist|
+    artist_count[artist] += 1 
+  end 
     artist_count
 end
 end
